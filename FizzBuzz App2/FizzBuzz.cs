98cdaa6d91fbe17114db43fc92bec8c9a32142e0
MarcinHir <hirek87@gmail.com>
2023-01-30 23:08:49 +0100

    Add project files.

---
+++ FizzBuzz App2/FizzBuzz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz_App2
+{
+    internal class FizzBuzz
+    {
+        public string GetNumber(int number)
+        {
+                    if (number % 3 == 0 && number % 5 == 0)
+                    {
+                        return "FizzBuzz";
+                    }
+
+                    else if (number % 3 == 0)
+                    {
+                        return "Fizz";
+                    }
+
+                    else if (number % 5 == 0)
+                    {
+                        return "Buzz";
+                    }
+
+                    else
+                        return $"Twoja liczba to {number}. Spróbuj jeszcze raz:";
+                }
+            }
+        }
+    
+
---
+++ FizzBuzz App2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz_App2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gra FizzBuzz wersja 2");
+            Console.WriteLine("Podaj liczbę całkowitą:");
+            FizzBuzz game = new FizzBuzz();
+
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                    Console.WriteLine("Miałeś podać liczbę całkowitą!!!");
+                else
+                {
+                    Console.WriteLine(game.GetNumber(number));
+                    break;
+                }
+
+
+            }
+
+        }
+    }
+}
